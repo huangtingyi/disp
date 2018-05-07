4e85e8e0c4169182f7280d5382130e0bf89769c1
hty-224 <4242817.qq.com>
2018-05-07 20:06:49 +0800

    add directory comm_supp 0.944+

---
+++ comm_supp/gta_supp.h
@@ -0,0 +1,30 @@
+#ifndef __GTA_SUPP_H__
+#define __GTA_SUPP_H__
+
+#include <cstring>
+#include <cstdint>
+#include "QTSStruct.h"
+#include "TDFAPIStruct.h"
+
+#ifndef MAX_STOCK_CODE
+#define MAX_STOCK_CODE	1000000
+#endif
+
+struct LimitUpDownStruct{
+	double	WarrantDownLimit;       ///< 股票跌停价格, （元）
+	double	WarrantUpLimit;         ///< 股票涨停价格, （元）
+};
+
+extern struct LimitUpDownStruct LIMIT[MAX_STOCK_CODE];
+
+void InitLimitArray();
+int64_t yuan2percentFen(const double yuan);
+char GTA2TDF_MARKET_STATUS(const char *TradeStatus, const char *SecurityPhaseTag);
+void GTA2TDF_SSEL2(const SSEL2_Quotation &src, TDF_MARKET_DATA &m, TDF_ORDER_QUEUE &qB, TDF_ORDER_QUEUE &qS);
+void GTA2TDF_SSEL2_T(const SSEL2_Transaction &src, TDF_TRANSACTION &t);
+void GTA2TDF_SZSEL2(const SZSEL2_Quotation &src, TDF_MARKET_DATA &m, TDF_ORDER_QUEUE &qB, TDF_ORDER_QUEUE &qS);
+void GTA2TDF_SZSEL2_T(const SZSEL2_Transaction &src, TDF_TRANSACTION &t);
+void GTA2TDF_SZSEL2_O(const SZSEL2_Order &src, TDF_ORDER &o);
+void GTA2TDF_SSEL2_AM(const SSEL2_Auction &src, TDF_MARKET_DATA &m);
+
+#endif
