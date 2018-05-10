270f0b9e6149812c1c7ae90a06c4eb1a5503675a
hty-224 <4242817.qq.com>
2018-05-10 13:44:48 +0800

    modify shell/startez.sh 0.948

--- shell/startez.sh
+++ shell/startez.sh
@@ -8,36 +8,35 @@ conf_file="$HOME/conf/config.ini"
 . $conf_file
 
 ethdev=${ethdev:-eno1}
-writeflag=${writeflag:-1}
 workroot=${workroot:-/stock/work}
 writeusr=${writeusr:-0}
 
+agent_file="$HOME/conf/agentcfg.json"
 disp_file="$HOME/conf/disp.json"
 user_file="$HOME/conf/user_privilege.json"
-tdf_file="$HOME/conf/ints_tdf.json"
 cfg_file="$HOME/conf/cfg.json"
 
-ints_tdf_bin="$HOME/bin/ints_tdf"
+agent_bin="$HOME/bin/agentcli"
 dat2cli_bin="$HOME/bin/dat2cli"
 moni_bin="$HOME/bin/moni.sh"
 pidof_bin="/usr/sbin/pidof"
 
-ints_tdf_log="$HOME/bin/log/ints_tdf_`date '+%Y%m%d'`.log"
+agentcli_log="$HOME/bin/log/agentcli_`date '+%Y%m%d'`.log"
 dat2cli_log="$HOME/bin/log/dat2cli_`date '+%Y%m%d'`.log"
 moni_log="$HOME/bin/log/moni_`date '+%Y%m%d'`.log"
 
-[ ! -f $tdf_file ] && echo "$tdf_file is not exist" && exit 1;
+[ ! -f $agent_file ] && echo "$agent_file is not exist" && exit 1;
 [ ! -f $cfg_file ] && echo "$cfg_file is not exist" && exit 1;
 [ ! -f $disp_file ] && echo "$disp_file is not exist" && exit 1;
 [ ! -f $user_file ] && echo "$user_file is not exist" && exit 1;
 
-[ ! -f $ints_tdf_bin ] && echo "$ints_tdf_bin is not exist" && exit 1;
+[ ! -f $agent_bin ] && echo "$agent_bin is not exist" && exit 1;
 [ ! -f $dat2cli_bin ] && echo "$dat2cli_bin is not exist" && exit 1;
 [ ! -f $moni_bin ] && echo "$moni_bin is not exist" && exit 1;
 [ ! -f $pidof_bin ] && echo "$pidof_bin is not exist" && exit 1;
 
 
-$pidof_bin -x ints_tdf && echo "ints_tdf is running" && exit 2;
+$pidof_bin -x agentcli && echo "agentcli is running" && exit 2;
 $pidof_bin -x dat2cli && echo "dat2cli is running" && exit 2;
 $pidof_bin -x moni.sh && echo "moni.sh is running" && exit 2;
 
@@ -68,12 +67,12 @@ EOF
 
 cd $HOME/bin
 
-nohup $ints_tdf_bin -w$writeflag -o$workroot -c$tdf_file -r$disp_file -u$user_file 1>$ints_tdf_log 2>&1 &
+nohup $agent_bin -p$agent_file -r$disp_file 1>$agentcli_log 2>&1 &
 sleep 1
 $pidof_bin -x ints_tdf
 if [ $? -ne 0 ]; then
-	echo "`date '+%Y/%m/%d %k:%M:%S'` ints_tdf is startup FAIL..";
-	echo "$ints_tdf_bin -w$writeflag -o$workroot -c$tdf_file -r$disp_file -u$user_file"
+	echo "`date '+%Y/%m/%d %k:%M:%S'` agentcli is startup FAIL..";
+	echo "$$agent_bin -p$agent_file -r$disp_file"
 	exit 3;
 fi
 
