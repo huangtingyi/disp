77be5727196fbdd8d3cb2954d8bfb26289cd2a7b
hty-224 <4242817.qq.com>
2018-05-23 16:11:56 +0800

    modify format add memtest.c to test/Makefile

--- test/broadcast_c.cpp
+++ test/broadcast_c.cpp
@@ -1,15 +1,14 @@
 /*
 *broadcast_client - 多播客户程序
 */
-#include        <netinet/in.h>
-#include        <errno.h>
-#include        <stdio.h>
-#include        <stdlib.h>
-#include        <sys/socket.h>
-#include        <string.h>
-#include        <arpa/inet.h>
-#include        <unistd.h>
-
+#include <netinet/in.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <unistd.h>
 
 #define MCAST_PROT 1234
 #define MCAST_ADDR "239.1.2.3"
@@ -18,70 +17,70 @@
 
 int main(int argc,char *argv[])
 {
-    int s;
-    struct sockaddr_in local_addr;
-    int err = -1;
+	int s;
+	struct sockaddr_in local_addr;
+	int err = -1;
 
-    s = socket(AF_INET, SOCK_DGRAM, 0);
-    if(s == -1)
-    {
-        perror("Socket()");
-        return -1;
-    }
+	s = socket(AF_INET, SOCK_DGRAM, 0);
+	if(s == -1)
+	{
+		perror("Socket()");
+		return -1;
+	}
 
-    memset(&local_addr, 0, sizeof(local_addr));
-    local_addr.sin_family = AF_INET;
-    local_addr.sin_addr.s_addr = htons(INADDR_ANY);
-    local_addr.sin_port = htons(MCAST_PROT);
+	memset(&local_addr, 0, sizeof(local_addr));
+	local_addr.sin_family = AF_INET;
+	local_addr.sin_addr.s_addr = htons(INADDR_ANY);
+	local_addr.sin_port = htons(MCAST_PROT);
 
-    err = bind(s, (struct sockaddr*)&local_addr,sizeof(local_addr));
-    if(err<0)
-    {
-        perror("bind()");
-        return -2;
-    }
+	err = bind(s, (struct sockaddr*)&local_addr,sizeof(local_addr));
+	if(err<0)
+	{
+		perror("bind()");
+		return -2;
+	}
 
 
-    int loop = 1;
-    err = setsockopt(s, IPPROTO_IP, IP_MULTICAST_LOOP, &loop, sizeof(loop));
-    if(err < 0)
-    {
-        perror("setsockopt():IP_MULTICAST_LOOP");
-        return -3;
-    }
+	int loop = 1;
+	err = setsockopt(s, IPPROTO_IP, IP_MULTICAST_LOOP, &loop, sizeof(loop));
+	if(err < 0)
+	{
+		perror("setsockopt():IP_MULTICAST_LOOP");
+		return -3;
+	}
 
-    struct ip_mreq mreq;
-    mreq.imr_multiaddr.s_addr = inet_addr(MCAST_ADDR);
-    mreq.imr_interface.s_addr = htons(INADDR_ANY);
+	struct ip_mreq mreq;
+	mreq.imr_multiaddr.s_addr = inet_addr(MCAST_ADDR);
+	mreq.imr_interface.s_addr = htons(INADDR_ANY);
 
-    err = setsockopt(s, IPPROTO_IP, IP_ADD_MEMBERSHIP, &mreq, sizeof(mreq));
-    if(err < 0)
-    {
-        perror("setsockopt():IP_ADD_MEMBERSHIP");
-        return -4;
-    }
+	err = setsockopt(s, IPPROTO_IP, IP_ADD_MEMBERSHIP, &mreq, sizeof(mreq));
+	if(err < 0)
+	{
+		perror("setsockopt():IP_ADD_MEMBERSHIP");
+		return -4;
+	}
 
-    int times = 0;
-    int addr_len = 0;
-    char buff[BUFF_SIZE];
-    int n = 0;
+	int times = 0;
+	int addr_len = 0;
+	char buff[BUFF_SIZE];
+	int n = 0;
 
-    for(times = 0; times <5; times++)
-    {
-        addr_len = sizeof(local_addr);
-        memset(buff, 0, BUFF_SIZE);
+	for(times = 0; times <5; times++)
+	{
+		addr_len = sizeof(local_addr);
+		memset(buff, 0, BUFF_SIZE);
 
-        n = recvfrom(s, buff, BUFF_SIZE, 0, (struct sockaddr*)&local_addr, (socklen_t*)&addr_len);
-        if(n == -1)
-        {
-            perror("recvform()");
-        }
+		n = recvfrom(s, buff, BUFF_SIZE, 0, (struct sockaddr*)&local_addr, (socklen_t*)&addr_len);
+		if(n == -1)
+		{
+			perror("recvform()");
+		}
 
-        printf("Recv %dst meesage from server:%s\n", times, buff);
-        sleep(MCAST_INTERVAL);
-    }
+		printf("Recv %dst meesage from server:%s\n", times, buff);
+		sleep(MCAST_INTERVAL);
+	}
 
-    err = setsockopt(s, IPPROTO_IP, IP_DROP_MEMBERSHIP,&mreq, sizeof(mreq));
-    close(s);
-    return 0;
+	err = setsockopt(s, IPPROTO_IP, IP_DROP_MEMBERSHIP,&mreq, sizeof(mreq));
+	close(s);
+	return 0;
 }
--- test/broadcast_s.cpp
+++ test/broadcast_s.cpp
@@ -1,14 +1,14 @@
 /*
 *broadcast_server - 多播服务程序
  */
-#include        <netinet/in.h>
-#include        <errno.h>
-#include        <stdio.h>
-#include        <stdlib.h>
-#include        <sys/socket.h>
-#include        <string.h>
-#include        <arpa/inet.h>
-#include        <unistd.h>
+#include <netinet/in.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <unistd.h>
 
 #define MCAST_PROT 1234
 #define MCAST_ADDR "239.1.2.3"
@@ -17,34 +17,34 @@
 
 int main(int argc,char *argv[])
 {
-    int s;
-    struct sockaddr_in mcast_addr;
-    s = socket(AF_INET, SOCK_DGRAM, 0);
-    if(s == -1)
-    {
-        perror("socket()");
-        return -1;
-    }
+	int s;
+	struct sockaddr_in mcast_addr;
+	s = socket(AF_INET, SOCK_DGRAM, 0);
+	if(s == -1)
+	{
+		perror("socket()");
+		return -1;
+	}
+	
+	memset(&mcast_addr, 0, sizeof(mcast_addr));
+	mcast_addr.sin_family = AF_INET;
+	mcast_addr.sin_addr.s_addr = inet_addr(MCAST_ADDR);
+	mcast_addr.sin_port = htons(MCAST_PROT);
 
-    memset(&mcast_addr, 0, sizeof(mcast_addr));
-    mcast_addr.sin_family = AF_INET;
-    mcast_addr.sin_addr.s_addr = inet_addr(MCAST_ADDR);
-    mcast_addr.sin_port = htons(MCAST_PROT);
+	while(1){
+		int n = sendto(s,
+			MCAST_DATA,
+			sizeof(MCAST_DATA),
+			0,
+			(struct sockaddr*)&mcast_addr,
+			sizeof(mcast_addr));
+		if(n < 0)
+		{
+			perror("sendto()");
+			return -2;
+		}
+		sleep(MCAST_INTERVAL);
+	}
 
-    while(1){
-        int n = sendto(s,
-            MCAST_DATA,
-            sizeof(MCAST_DATA),
-            0,
-            (struct sockaddr*)&mcast_addr,
-            sizeof(mcast_addr));
-        if(n < 0)
-        {
-            perror("sendto()");
-            return -2;
-        }
-        sleep(MCAST_INTERVAL);
-    }
-
-    return 0;
+	return 0;
 }
